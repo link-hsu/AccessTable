085d50246cd2dcc649610ae47d6521a68e50f9e5
link-hsu <88920300+link-hsu@users.noreply.github.com>
2025-05-19 17:51:43 +0800

    Create Table15A.sql

---
+++ 20250507/台幣/sql/央行/Table15A.sql
@@ -0,0 +1,18 @@
+1.需要PNCDAL欄位
+Period Or Code
+LastMonthBalance
+InterestRate
+IssueAmount
+Repayment
+
+2.
+先Check欄位順序，
+寫一迴圈，當欄位 Period Or Code 與資料表欄位相等時，
+逐一使用offset將資料填入
+
+欄位設定弄一個表
+
+PNCD設定弄一個表
+
+
+
